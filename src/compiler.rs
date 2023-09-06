89a796db3570c8357812cebb1512a155cc5793cd
Octavian Oncescu <octavian.oncescu@gmail.com>
2023-09-06 13:03:00 +0200

    Handle part of function definition compiler states

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,13 +1,83 @@
+use crate::lexer::{Token, Position, TokenKind, Symbol, Keyword};
+
 pub struct Compiler {
+    /// Compiler state
     state: CompilerState,
+
+    /// Buffer for the main function
+    out_main: Vec<u8>,
+
+    /// Buffer for other functions
+    out_funcs: Vec<Vec<u8>>,
+
+    /// Buffer for the bitmap
+    out_bitmap: Vec<u8>,
 }
 
 impl Compiler {
     pub fn new() -> Self {
         Self {
             state: CompilerState::Any,
+            out_main: vec![],
+            out_funcs: vec![],
+            out_bitmap: vec![],
         }
     }
+
+    pub fn push_token(&mut self, token: Token) -> Result<(), CompilerErr> {
+        match (&self.state, token.kind) {
+            (&CompilerState::Any, TokenKind::Keyword(Keyword::Function)) => {
+                self.state = CompilerState::ExpectingFuncIdentifier;
+            }
+
+            // We expected a function identifier
+            (&CompilerState::Any, _) => {
+                return Err(CompilerErr::ExpectedFunctionDefinition(token.position.clone()));
+            }
+
+            // Main function identifier
+            (&CompilerState::ExpectingFuncIdentifier, TokenKind::Identifier(identifier)) if identifier.as_str() == "main" => {
+                self.state = CompilerState::ExpectingMainFuncLeftParanthesis;
+            }
+
+            // Other function identifier
+            (&CompilerState::ExpectingFuncIdentifier, TokenKind::Identifier(identifier)) if identifier.as_str() != "main" => {
+                self.state = CompilerState::ExpectingFuncLeftParanthesis; 
+            }
+
+            (&CompilerState::ExpectingFuncIdentifier, _) => {
+                return Err(CompilerErr::ExpectedIdentifier(token.position.clone()));
+            }
+
+            (&CompilerState::ExpectingMainFuncLeftParanthesis, TokenKind::Symbol(Symbol::ParenthesisLeft)) => {
+                self.state = CompilerState::ExpectingMainFuncMalleableOrIdentifier;
+            }
+
+            (&CompilerState::ExpectingFuncLeftParanthesis, TokenKind::Symbol(Symbol::ParenthesisLeft)) => {
+                self.state = CompilerState::ExpectingFuncArgIdentifier;
+            }
+
+            (&CompilerState::ExpectingFuncLeftParanthesis, _) => {
+                return Err(CompilerErr::ExpectedParanthesisLeft(token.position.clone()));
+            }
+
+            _ => unimplemented!()
+        }
+
+
+        Ok(())
+    }
+
+    pub fn compile(&self) -> Vec<u8> {
+        unimplemented!();
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum CompilerErr {
+    ExpectedFunctionDefinition(Position),
+    ExpectedIdentifier(Position),
+    ExpectedParanthesisLeft(Position),
 }
 
 enum CompilerState {
@@ -26,15 +96,34 @@ enum CompilerState {
     /// We hit a `function` definition, now we want the identifier.
     ExpectingFuncIdentifier,
 
+    /// We hit the main function identifier, now we want the left paranthesis for the arguments.
+    ExpectingMainFuncLeftParanthesis,
+
+    /// We hit the main function paranthesis for arguments, now we want the actual 
+    /// arguments which can be malleable or not.
+    ExpectingMainFuncMalleableOrIdentifier,
+
+    /// We hit a malleable arg definition, now we want the identifier.
+    ExpectingMainFuncMalleableIdentifier,
+
+    /// We have hit the main function identifier, now we either expect a colon,
+    /// in order to specify the argument type, a comma, or the right paranthesis
+    /// to end the main function definitions.
+    ExpectingMainFuncColonCommaOrRightParanthesis,
+
+    /// We finished the arguments definition, now we want the start brace
+    /// of the main function body.
+    ExpectingMainFuncBrace,
+
+    /// We hit the start brace of the function body, now we want the actual body.
+    ExpectingMainFuncBody,
+
     /// We hit the function identifier, now we want the left paranthesis for the arguments.
     ExpectingFuncLeftParanthesis,
 
     /// We hit the function paranthesis for arguments, now we want the actual 
-    /// arguments which can be malleable or not.
-    ExpectingFuncMalleableOrIdentifier,
-
-    /// We hit a malleable arg definition, now we want the identified.
-    ExpectingFuncMalleableIdentifier,
+    /// arguments.
+    ExpectingFuncArgIdentifier,
 
     /// We have hit the function identifier, now we either expect a colon,
     /// in order to specify the argument type, a comma, or the right paranthesis
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use glob::glob;
 use std::fs;
-use crate::{lexer::tokenise, ast::{Function, Program}};
+use crate::lexer::tokenise;
+use crate::compiler::Compiler;
 
 fn main() {
-    let mut tokens: Vec<Function> = vec![];
+    let mut tokens: Vec<_> = vec![];
+    let mut out_script: Vec<u8> = vec![];
 
     for entry in glob("./**/*.ps").expect("Failed to read .ps files") {
         match entry {
             Ok(path) => {
                 let contents = fs::read_to_string(path)
                     .expect("Should have been able to read the file");
-                let r: Vec<_> = tokenise(&contents).collect();
-                tokens.push(r.into());
+                tokens.extend(tokenise(&contents));
             },
             Err(e) => println!("{:?}", e),
         }
     }
-    let program: Program = tokens.into();
-    println!("{:#?}", program);
+
+    let mut compiler = Compiler::new();
+
+    for t in tokens {
+        match compiler.push_token(t) {
+            Ok(()) => { },
+            Err(err) => {
+                println!("Compiler err: {:?}", err); // TODO: Pretty errors
+            } 
+        }
+    }
+
+    println!("Compiled successfuly! Output: \n\n{}", hex::encode(compiler.compile()));
 }
 
 mod lexer;
 mod compiler;
-mod ast;
